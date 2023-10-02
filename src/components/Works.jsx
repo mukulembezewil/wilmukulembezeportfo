2ebc1b70c418ba226123eae5d0349a8fe63da06d
mukulembezewil <wilmukulembeze@gmail.com>
2023-10-02 18:50:57 +0300

    Edit styles.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const Section = styled.div`
 	height: 100vh;
 	scroll-snap-align: center;
-	background-color: #422c58;
 `;
 const Works = () => {
 	return <Section>Works</Section>;
