b780bf942e5f2318745a3509aa840f10466b89f1
mukulembezewil <wilmukulembeze@gmail.com>
2023-10-02 11:53:46 +0300

    Create contact component (section)

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Section = styled.div`
+	height: 100vh;
+	scroll-snap-align: center;
+	background-color: #0c84ee;
+`;
+
+const Contact = () => {
+	return <Section>Contact</Section>;
+};
+export default Contact;
