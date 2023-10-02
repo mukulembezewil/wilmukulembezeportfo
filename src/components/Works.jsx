ed7cdcdff60dc5f6f91d7dfff98caa19d4c3f0b0
mukulembezewil <wilmukulembeze@gmail.com>
2023-10-02 11:54:47 +0300

    Create Works component(section)

---
+++ src/components/Works.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Section = styled.div`
+	height: 100vh;
+	scroll-snap-align: center;
+	background-color: #422c58;
+`;
+const Works = () => {
+	return <Section>Works</Section>;
+};
+export default Works;
