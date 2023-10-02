b7db3c46b6ab8c03a2efc4b4c446a3c4554aa3c6
mukulembezewil <wilmukulembeze@gmail.com>
2023-10-02 11:54:20 +0300

    Create who component(section)

---
+++ src/components/Who.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Section = styled.div`
+	height: 100vh;
+	background-color: #755892;
+	scroll-snap-align: center;
+`;
+const Who = () => {
+	return <Section>Who</Section>;
+};
+export default Who;
