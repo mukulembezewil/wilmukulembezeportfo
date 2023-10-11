d3336daedf67c8f8b15a0d186c247ec3bc5a6812
mukulembezewil <wilmukulembeze@gmail.com>
2023-10-11 15:47:44 +0300

    Change background color.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Section = styled.div`
 	height: 100vh;
 	scroll-snap-align: center;
+	background-color: #af2bbf;
 `;
 const Works = () => {
 	return <Section>Works</Section>;
