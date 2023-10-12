ea2b1163c6aa806fd914433e48d833b91d304c80
mukulembezewil <wilmukulembeze@gmail.com>
2023-10-12 15:05:13 +0300

    Edit navbar container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,66 +1,47 @@
 import styled from 'styled-components';
 
-const Section = styled.div`
-	display: flex;
-	justify-content: center;
-`;
-
 const Container = styled.div`
+	background-color: black;
 	position: fixed;
+	height: 50px;
 	width: 100%;
 	display: flex;
-	justify-content: space-between;
+	justify-content: space-evenly;
 	align-items: center;
 `;
 
 const Links = styled.div`
 	display: flex;
-	align-items: center;
-	gap: 50px;
-`;
-
-const Icons = styled.div`
-	display: flex;
-	align-items: center;
-	gap: 20px;
+	/* align-items: center; */
+	/* gap: 50px; */
 `;
 
 const Logo = styled.img`
-	width: 160px;
+	width: 120px;
 `;
 
 const List = styled.ul`
 	display: flex;
 	gap: 20px;
 	list-style: none;
-	padding-top: 42px;
-	padding-left: 75px;
+	/* padding-top: 42px; */
+	/* padding-left: 75px; */
 	color: #8a8888;
-	font-size: 14px;
+	font-size: 12px;
 `;
 
 const ListItem = styled.li`
 	cursor: pointer;
 `;
 
-const Icon = styled.img`
-	padding-top: 30px;
-	width: 20px;
-	cursor: pointer;
-`;
-
 const Button = styled.button`
-	margin-top: 20px;
-	width: 100px;
-	height: 30px;
-	background: #5899e2;
-	/* background: rgb(218, 165, 32);
-	background: linear-gradient(
-		90deg,
-		rgba(218, 165, 32, 1) 0%,
-		rgba(242, 215, 31, 1) 35%,
-		rgba(237, 149, 26, 1) 100%
-	); */
+	/* margin-top: 20px; */
+	width: 80px;
+	height: 20px;
+	font-size: 12px;
+	color: #bad3f0;
+	background: rgb(218, 165, 32);
+	background: linear-gradient(90deg, #535353 0%, #353333 35%, #2e2d2d 100%);
 	border: none;
 	border-radius: 10px 0px 10px 0px;
 	cursor: pointer;
@@ -68,24 +49,24 @@ const Button = styled.button`
 
 const Navbar = () => {
 	return (
-		<Section>
-			<Container>
-				<Links>
-					<Logo src="./img/logo.png" />
-					<List>
-						<ListItem>Home</ListItem>
-						<ListItem>ReactJS</ListItem>
-						<ListItem>NodeJS</ListItem>
-						<ListItem>VueJS</ListItem>
-						<ListItem>Contact</ListItem>
-					</List>
-				</Links>
-				<Icons>
-					<Icon src="./img/search.png" />
-					<Button>Hire Now</Button>
-				</Icons>
-			</Container>
-		</Section>
+		// <Section>
+		<Container>
+			<Logo src="./img/logo.png" />
+			<Links>
+				<List>
+					<ListItem>Home</ListItem>
+					<ListItem>ReactJS</ListItem>
+					<ListItem>NodeJS</ListItem>
+					<ListItem>VueJS</ListItem>
+					<ListItem>JavaScript</ListItem>
+					<ListItem>CSS</ListItem>
+					<ListItem>Contact</ListItem>
+				</List>
+			</Links>
+			<Button>Hire Now</Button>
+			{/* <Icons></Icons> */}
+		</Container>
+		// </Section>
 	);
 };
 export default Navbar;
