e3ade0945072fe6bdc0ce848d145b0c08e62a35b
mukulembezewil <wilmukulembeze@gmail.com>
2023-10-18 15:36:49 +0300

    Build effects

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '/wilmukulembezeportfo/',
-	build: {
-		outDir: 'build',
-	},
+	// build: {
+	// 	outDir: 'build',
+	// },
 	plugins: [react()],
 });
